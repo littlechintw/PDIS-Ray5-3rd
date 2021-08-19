06e070ed674fa4b16e34fa2208bae85385feac8e
littlechin <littlechintw@gmail.com>
2021-08-19 15:50:16 +0800

    Add bar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
-<template>
-  <div id="app">
-    <div id="nav">
+<template class="ma-0 pa-0">
+  <div id="app" class="ma-0 pa-0">
+    <Bar />
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div> -->
+
+    <router-view />
   </div>
 </template>
+
+<script>
+import Bar from "@/components/Bar.vue";
+
+export default {
+  components: {
+    Bar,
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
+  padding: 0px;
 }
 
 #nav {
@@ -28,5 +41,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.container {
+  max-width: 100vw;
+  padding: 0px;
 }
 </style>
---
+++ src/components/Bar.vue
@@ -0,0 +1,216 @@
+<template>
+  <div class="bar">
+    <div class="bar_data">
+      <a class="bar_left" style="color: #4f4f4f; font-size: 14px">聯絡我們</a>
+      <div class="bar_right">
+        <a style="margin-left: 20px; color: #4f4f4f; font-size: 14px"
+          >網頁操作教學</a
+        >
+        <a style="margin-left: 20px; color: #4f4f4f; font-size: 14px"
+          >常見問答</a
+        >
+        <a style="margin-left: 20px; color: #4f4f4f; font-size: 14px"
+          >登入 / 註冊</a
+        >
+      </div>
+
+      <br />
+      <v-divider style="margin-top: 15px" />
+
+      <br />
+      <div class="bar_with_logo">
+        <v-img
+          height="72px"
+          width="280px"
+          src="../assets/img/logo.png"
+          style="float: left"
+        />
+
+        <!-- <div class="bar_with_logo">
+          <a style="font-size: 24px; font-weight: bold">桃園市志願服務</a>
+          <br />
+          <a style="font-size: 16px">整合資訊平台</a>
+        </div> -->
+
+        <div class="navbar">
+          <div class="dropdown" v-for="item in nav_bar_items" :key="item">
+            <button class="dropbtn">
+              {{ item.title }}
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a
+                href="#"
+                v-for="dropdown_item in item.dropdown"
+                :key="dropdown_item"
+                >{{ dropdown_item.title }}</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="bar_right">
+          <v-row align="center" justify="space-around">
+            <v-btn tile color="gray">
+              <v-icon left> mdi-t </v-icon>
+            </v-btn>
+          </v-row>
+        </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bar",
+  props: {},
+  data: () => ({
+    nav_bar_items: [
+      {
+        title: "公告專區",
+        dropdown: [{ title: "最新消息" }, { title: "教育訓練" }],
+      },
+      {
+        title: "我要當志工",
+        dropdown: [
+          { title: "志工招募訊息" },
+          { title: "參加教育訓練" },
+          { title: "服務類別簡介" },
+        ],
+      },
+      {
+        title: "我要用志工",
+        dropdown: [
+          { title: "如何成為運行單位" },
+          { title: "志工教育訓練" },
+          { title: "志工管理操作教學" },
+        ],
+      },
+      {
+        title: "下載專區",
+        dropdown: [{ title: "檔案下載" }, { title: "相關法規" }],
+      },
+      {
+        title: "分享專區",
+        dropdown: [
+          { title: "志工分享" },
+          { title: "電子刊物" },
+          { title: "活動花絮" },
+          { title: "相關連結" },
+        ],
+      },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+.bar {
+  width: 100%;
+  height: 174px;
+  background-color: white;
+}
+
+.bar_data {
+  padding-top: 30px;
+  padding-left: 130px;
+  padding-right: 130px;
+}
+
+.bar_left {
+  float: left;
+}
+
+.bar_right {
+  float: right;
+}
+
+.full_width {
+  width: 100%;
+}
+
+.black_block {
+  background-color: black;
+}
+
+/* Solid border */
+hr.solid {
+  border-top: 1px solid #dedede;
+}
+
+.bar_with_logo {
+  vertical-align: middle;
+}
+
+/* Navbar container */
+.navbar {
+  overflow: hidden;
+  padding-top: 15px;
+  padding-left: 100px;
+}
+
+/* Links inside the navbar */
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* The dropdown container */
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+/* Dropdown button */
+.dropdown .dropbtn {
+  font-size: 16px;
+  border: none;
+  outline: none;
+  color: black;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit; /* Important for vertical align on mobile phones */
+  margin: 0; /* Important for vertical align on mobile phones */
+}
+
+/* Add a red background color to navbar links on hover */
+.navbar a:hover,
+.dropdown:hover .dropbtn {
+  background-color: #f9f9f9;
+}
+
+/* Dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+/* Add a grey background color to dropdown links on hover */
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
